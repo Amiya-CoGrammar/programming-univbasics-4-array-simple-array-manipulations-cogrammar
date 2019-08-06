bf5fc167e4b17a96a57e9d6d83bb55b4b95a1358
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-06 20:01:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,7 +57,7 @@ def using_delete(array, string)
 end
 using_delete(["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"],"Steven")
 
-def using_delete_at(array, 2)
+def using_delete_at(array)
   array.delete_at()
 end
 using_delete_at(["Johnny 5", "R2D2", "Robocop"])
