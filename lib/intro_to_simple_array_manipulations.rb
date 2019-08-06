79739b7f2c8c35864589ce37b07d169af5136972
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-06 19:32:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,6 +37,6 @@ end
 using_concat(["raindrops on roses", "whiskers on kittens"], ["sports cars", "flatiron school"])
 
 def using_insert(array)
-  array.insert(3, 'Python')
+  array.insert(4, 'Python')
 end
 using_insert( ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"])
